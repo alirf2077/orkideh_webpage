438cde4f20d48f43e79aa8c692ad7472bac4db52
alirf2077 <alirf2077@gmail.com>
2025-05-10 23:04:09 +0330

    depoy_3 version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        fontFamily: {
-          INDEPENDENT: ["INDEPENDENT"],
-      },
+      //   fontFamily: {
+      //     INDEPENDENT: ["INDEPENDENT"],
+      // },
     },
   },
   variants: {
